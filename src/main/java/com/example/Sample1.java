cdc44e0798ac6080e772c371a2752c965d629fae
VuHoHaiTrieu <trieuhaimoi0305@gmail.com>
2025-07-31 17:21:35 +0700

    ✅ Sửa nội dung file Sample1.java: In ra 'Java 1'

--- src/main/java/com/example/Sample1.java
+++ src/main/java/com/example/Sample1.java
@@ -1,1 +1,5 @@
-public class Sample1 { public static void main(String[] args) { System.out.println("Java 1"); }}
+public class Sample1 {
+    public static void main(String[] args) {
+        System.out.println("Java 1");
+    }
+}
